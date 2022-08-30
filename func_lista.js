c8319b505399d0b0e4ec0db01d0f1270d8c1622d
PaAdRaAg <pramos12@ucol.mx>
2022-08-29 19:41:59 -0500

    markdo funcionando

--- func_lista.js
+++ func_lista.js
@@ -2,51 +2,6 @@ let input = document.querySelector('.text-tareas');
 let agBtn = document.querySelector('.btn-agregar-tarea');
 let tareas = document.querySelector('.tareas')
 
-/*
-//activar boton de agragar después de ingresar algún valor a la caja de texto
-input.addEventListener('keyup', () => {
-    if(input.value.trim() !== 0){
-        agBtn.classList.add('active');
-    } else{
-        agBtn.classList.remove('active');
-    };
-});
-
-//agregar nueva tarea a la lista
-agBtn.addEventListener('click', () =>{
-    if(input.value.trim() != 0){
-        let nuevaTarea = document.createElement('div');
-        nuevaTarea.classList.add('tarea');
-        nuevaTarea.innerHTML = `
-        <p> ${input.value} </p>
-        <div class="nuevaTarea-btn">
-        <i class="fa-solid fa-check nuevaTarea-btn-done"></i>            
-        <i class="fa-solid fa-xmark nuevaTarea-btn-delete"></i>
-        </div>
-        `
-        tareas.appendChild(nuevaTarea);
-        input.value = '';
-        agBtn.classList.remove('active');
-    } else{
-        alert("Por favor ingrese una tarea")
-    };
-});
-
-//eliminar tareas
-tareas.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('fa-xmark')){
-        e.target.parentElement.parentElement.remove();
-    };
-});
-
-//marcar tareas completadas
-tareas.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('fa-check')){
-        e.target.parentElement.parentElement.classList.toggle('completada');
-    };
-});
-*/
-/*----------------------------------------------------------------------------------------*/
 input.addEventListener('keyup', () => {
     if(input.value.trim() !== 0){
         agBtn.classList.add('active');
@@ -66,14 +21,13 @@ agBtn.addEventListener('click', function (){
         listareas.push(input.value);
         localStorage.setItem('localItem', JSON.stringify(listareas)); 
     };
-        showItem();
+    showItem();
 });
 
 function showItem(){
     let localItems = JSON.parse( localStorage.getItem('localItem'));
     if(localItems === null){
         listareas = [];
-    
     }else{
         listareas = localItems;
     };
@@ -83,16 +37,15 @@ function showItem(){
     listareas.forEach((data, index )=> {
     html += `
     <div class="tarea">
-    <p> ${data} </p>
+    ${data}
     <div class="nuevaTarea-btn">
-    <i class="fa-solid fa-check nuevaTarea-btn-done" onClick= "markDo(${index}")></i>            
+    <i class="fa-solid fa-check nuevaTarea-btn-done" onClick="markDo(${index})"></i>            
     <i class="fa-solid fa-xmark nuevaTarea-btn-delete" onClick="deleteItem(${index})"></i>
     </div>
     </div>
     `
     input.value = '';
     agBtn.classList.remove('active');
-
 });
 itemShow.innerHTML = html;
 }; 
@@ -105,18 +58,16 @@ function deleteItem(index){
     showItem();
 };
 
-function markDo(index){
-    let localItems = JSON.parse(localStorage.getItem('localItem'));
-
-    index.parentElement.parentElement.classList.toggle('completada');
-
-    localStorage.setItem('localItem', JSON.stringify(listareas));
+function markDo(index) {
+    if (listareas[index].includes('<strike>')){
+        listareas[index] = listareas[index].replace('<strike>', "");
+    } else {
+        listareas[index] = '<strike>' + listareas[index] + '</strike>';
+    };
+    if (localStorage.getItem('localItem') == null){
+      localStorage.setItem('localItem', JSON.stringify(listareas));
+    } else {
+      localStorage.setItem('localItem', JSON.stringify(listareas));
+    };
     showItem();
 };
-/*
-tarea.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('fa-check')){
-        e.target.parentElement.parentElement.classList.toggle('completada');
-    };
-});*/
-
