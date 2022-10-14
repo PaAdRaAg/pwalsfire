82ff8a2087feecd76fd3cdc8a1059dba1554e4c3
PaAdRaAg <pramos12@ucol.mx>
2022-10-13 21:23:58 -0500

    regist sw updt

--- regist_serviceWorker.js
+++ regist_serviceWorker.js
@@ -1,21 +1,3 @@
-/*if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('./Service_Worker.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err));
-}
-
-window.addEventListener('load', async () => {
-    if ('serviceWorker' in navigator) {
-    try {
-      await navigator.serviceWorker.register('./Service_Worker.js');
-      console.log('Registro de SW exitoso');
-    } catch (err) {
-      console.log('Registro de SW Fallido', err);
-    }
-    }
-});
-*/
-
 window.addEventListener('load', async () => {
     if ('serviceWorker' in navigator) {
       navigator.serviceWorker.register('./Service_Worker.js')
