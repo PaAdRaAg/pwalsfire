9de4b15b91a33b47a49d59c128d8074d2339e3b1
PaAdRaAg <pramos12@ucol.mx>
2023-01-23 12:54:00 -0600

    cndk

--- Service_Worker.js
+++ Service_Worker.js
@@ -62,3 +62,13 @@ self.addEventListener('fetch', e => {
         })
     )
 });
+
+self.addEventListener('sync', function (event) {
+    if (event.id == 'update-leaderboard') {
+      event.waitUntil(
+        caches.open('mygame-dynamic').then(function (cache) {
+          return cache.add('/leaderboard.json');
+        }),
+      );
+    }
+  });
