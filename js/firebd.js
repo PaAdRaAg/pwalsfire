e5eed8079bf8e3fe0d43a2e31663ff880cb8e77b
PaAdRaAg <pramos12@ucol.mx>
2023-01-24 15:52:58 -0600

    empezar sincro

--- js/firebd.js
+++
@@ -1,8 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("path/to/serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://pwals-e5335-default-rtdb.firebaseio.com"
-});
