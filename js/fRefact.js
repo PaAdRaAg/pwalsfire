7394219c3df9878a62a485019989cfc0ee12cc7a
PaAdRaAg <pramos12@ucol.mx>
2023-01-27 17:10:11 -0600

    avance a liminar y marcar

--- js/fRefact.js
+++ js/fRefact.js
@@ -22,37 +22,6 @@ let input = document.querySelector('.text-tareas');
 let agBtn = document.querySelector('.btn-agregar-tarea');
 let tareas = document.querySelector('.tareas');
 
-// 2 Mostrar tareas (de db) (y actualizar ls en base a db)
-// localStorage.clear();
-db.collection("Usuario").onSnapshot((querySnapshot) => {
-    tareas.innerHTML = "";
-    localStorage.clear();
-
-    querySnapshot.forEach((doc) => {
-        let localItems = JSON.parse(localStorage.getItem('localItem'));
-        if(localItems === null){
-            listareas = [];
-        }
-        else{
-            listareas = localItems;
-        }
-        listareas.push(doc.data().tarea);
-        console.log("Registro de tarea el ls exitoso");
-        localStorage.setItem('localItem', JSON.stringify(listareas)); 
-
-        tareas.innerHTML += `
-        <div class="tarea">
-            ${doc.data().tarea}
-            <div class="nuevaTarea-btn">
-                <i class="fa-solid fa-check nuevaTarea-btn-done" onclick="markDo(${doc.id})"></i>            
-                <i class="fa-solid fa-xmark nuevaTarea-btn-delete" onclick="deleteItem(${doc.id})"></i>
-            </div>
-        </div>
-        `
-    });
-
-});
-
 // 1 Agregar tareas (a db)
 //Habilitar / deshabilitar botón de agregar
 input.addEventListener('keyup', () => {
@@ -62,6 +31,7 @@ input.addEventListener('keyup', () => {
         agBtn.classList.remove('active');
     }
 });
+
 //Evento para enviar agregar tarea con tecla enter
 input.addEventListener('keydown', e =>{
     if(e.keyCode === 13){
@@ -71,6 +41,7 @@ input.addEventListener('keydown', e =>{
             db.collection("Usuario").add({
                 tarea: input.value,
                 status: "activa",
+                timestamp: firebase.firestore.Timestamp.fromDate(new Date())
             })
             .then((docRef) => {
                 console.log("Adición exitosa || ID: " + docRef.id + " Tarea: " + tarea);
@@ -97,7 +68,7 @@ agBtn.addEventListener('click', function (){
         let tarea = input.value;
         db.collection("Usuario").add({
             tarea: input.value,
-            status: "active",
+            status: "activa",
             timesamp: firebase.firestore.FieldValue.serverTimestamp()
         })
         .then((docRef) => {
@@ -111,10 +82,95 @@ agBtn.addEventListener('click', function (){
         agBtn.classList.remove('active');
         alert("Ingrese alguna tarea para agregar.");
     }
-    input.value = ""
+    input.value = "";
 });
 
 // 3 Marcar como hechas (cambiar status en db)
+function markDo(id){
+
+    db.collection("Usuario").doc(id).update({
+        status: "completed"
+    })
+    // item.get().then(function(doc) {
+    //     if (doc.exists) {
+    //         console.log("pasó 1")
+    //         if(doc.data().status == "activa"){
+    //             console.log("pasó 2")
+    //             item.update({
+    //                 status: "completed"
+    //             })
+    //         } else {
+    //             item.update({
+    //                 status: "activa"
+    //             })
+    //         }
+    //     }
+    // })
+
+    // let item = db.collection("Usuario").doc(id);
+    // item.get().then(function(doc) {
+    //     if (doc.exists) {
+    //         console.log("pasó 1")
+    //         if(doc.data().status == "activa"){
+    //             console.log("pasó 2")
+    //             item.update({
+    //                 status: "completed"
+    //             })
+    //         } else {
+    //             item.update({
+    //                 status: "activa"
+    //             })
+    //         }
+    //     }
+    // })
+}
+
+// .completed {
+//     text-decoration: line-through;
+// }
+
+// .unCompleted {
+//     text-decoration: none;
+// }
 
 // 4 Eliminar tarea (de db)
+function deleteItem(){
+    db.collection("Usuario").doc(id).delete().then(() => {
+        console.log("Document successfully deleted!");
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+}
+
+
+
+// 2 Mostrar tareas (de db) (y actualizar ls en base a db)
+// db.collection("citas_registradas").orderBy("fecha", "desc")
+db.collection("Usuario").orderBy("timestamp", "asc").onSnapshot((querySnapshot) => {
+    tareas.innerHTML = "";
+    localStorage.clear();
+
+    querySnapshot.forEach((doc) => {
+        let localItems = JSON.parse(localStorage.getItem('localItem'));
+        if(localItems === null){
+            listareas = [];
+        }
+        else{
+            listareas = localItems;
+        }
+        listareas.push(doc.data().tarea);
+        console.log("Registro de tarea el ls exitoso");
+        localStorage.setItem('localItem', JSON.stringify(listareas)); 
+
+        tareas.innerHTML += `
+        <div class="tarea">
+            ${doc.data().tarea}
+            <div class="nuevaTarea-btn">
+                <i class="fa-solid fa-check nuevaTarea-btn-done" onclick="markDo(toString(doc.id))"></i>            
+                <i class="fa-solid fa-xmark nuevaTarea-btn-delete" onclick="deleteItem(${doc.id})"></i>
+            </div>
+        </div>
+        `
+    });
+});
 
